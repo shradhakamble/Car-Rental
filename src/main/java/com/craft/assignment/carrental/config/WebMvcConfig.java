00ce0f957c8c85339c9721150bbec3b17d8cca2a
shradha <shradhakamble1808@gmail.com>
2023-06-07 23:00:50 +0530

    cleanup for token generation and validation

--- src/main/java/com/craft/assignment/carrental/config/WebMvcConfig.java
+++ src/main/java/com/craft/assignment/carrental/config/WebMvcConfig.java
@@ -1,7 +1,6 @@
 package com.craft.assignment.carrental.config;
 
 import com.craft.assignment.carrental.interceptors.TokenValidationInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,7 +10,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     private final TokenValidationInterceptor tokenValidationInterceptor;
 
-    @Autowired
     public WebMvcConfig(TokenValidationInterceptor tokenValidationInterceptor) {
         this.tokenValidationInterceptor = tokenValidationInterceptor;
     }
--- src/main/java/com/craft/assignment/carrental/interceptors/TokenValidationInterceptor.java
+++ src/main/java/com/craft/assignment/carrental/interceptors/TokenValidationInterceptor.java
@@ -2,7 +2,6 @@ package com.craft.assignment.carrental.interceptors;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -11,18 +10,17 @@ public class TokenValidationInterceptor implements HandlerInterceptor {
 
     private final TokenValidator tokenValidator;
 
-    @Autowired
     public TokenValidationInterceptor(TokenValidator tokenValidator) {
         this.tokenValidator = tokenValidator;
     }
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         boolean validToken = tokenValidator.validateToken(request);
         if (validToken) {
             return true; // Token is valid, allow the request to proceed
         } else {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); //return 401 unauthorized
             return false; // Token is invalid, deny the request
         }
     }
