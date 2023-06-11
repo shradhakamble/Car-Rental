5ca4f9bf61e43f65b2e3c0bb4736b749b55c1497
shradha <shradhakamble1808@gmail.com>
2023-06-11 20:09:44 +0530

    cleanup for token generation and validation

--- src/main/resources/db/migration/V2__create_constraints_indexes.sql
+++ src/main/resources/db/migration/V2__create_constraints_indexes.sql
@@ -1,9 +1,3 @@
-ALTER TABLE driver_vehicle_infoset
-ADD CONSTRAINT fk_vehicles_driver_id
-FOREIGN KEY (driver_id)
-REFERENCES driver_infoset (id);
-
-
 ALTER TABLE driver_onboarding_journey
 ADD CONSTRAINT fk_journey_driver_id
 FOREIGN KEY (driver_id)
@@ -26,12 +20,6 @@ ON driver_infoset (email);
 CREATE INDEX idx_driver_status
 ON driver_infoset (status);
 
-
-CREATE INDEX idx_vehicle_number
-ON driver_vehicle_infoset (vehicle_number);
-
-CREATE INDEX idx_vehicle_driver
-ON driver_vehicle_infoset (driver_id);
 
 CREATE INDEX idx_journey_driver
 ON driver_onboarding_journey (driver_id);
